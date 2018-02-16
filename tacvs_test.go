d32e8dae5d687e83fe8775f11a6bcda37f999447
Pawel Knap <pawelknap88@gmail.com>
2018-02-16 06:04:21 +0100

    tensor: remove flacky sizeOf function from tests

--- tacvs_test.go
+++ tacvs_test.go
@@ -11,62 +11,58 @@ func TestTensorInfo(t *testing.T) {
 	tests := []struct {
 		Tensor   *tacvs.Tensor
 		Shape    []int
+		Size     int
 		DataSize int
 	}{
 		{
 			// 0 //
 			Tensor:   tacvs.NewTensor(1, 1),
 			Shape:    []int{1},
+			Size:     1,
 			DataSize: 1,
 		},
 		{
 			// 1 //
 			Tensor:   tacvs.NewTensor(1, 2, 3, 4),
 			Shape:    []int{1, 2, 3, 4},
+			Size:     24,
 			DataSize: 24,
 		},
 		{
 			// 2 //
 			Tensor:   tacvs.NewTensor(1, 1, 1, 1),
 			Shape:    []int{1},
+			Size:     1,
 			DataSize: 1,
 		},
 		{
 			// 3 //
 			Tensor:   tacvs.NewTensor(6, 1, 1, 1, 1),
 			Shape:    []int{6},
+			Size:     6,
 			DataSize: 6,
 		},
 		{
 			// 4 //
 			Tensor:   tacvs.NewTensor(),
 			Shape:    []int{},
+			Size:     0,
 			DataSize: 0,
 		},
 		{
 			// 5 //
 			Tensor:   &tacvs.Tensor{},
 			Shape:    []int{},
+			Size:     0,
 			DataSize: 0,
 		},
 		{
 			// 6 //
 			Tensor:   tacvs.NewTensor(2),
 			Shape:    []int{2},
+			Size:     2,
 			DataSize: 2,
 		},
-	}
-
-	sizeOf := func(shape []int) int {
-		if len(shape) == 0 {
-			return 0
-		}
-
-		size := 1
-		for i := 0; i < len(shape); i++ {
-			size *= shape[i]
-		}
-		return size
 	}
 
 	for i, test := range tests {
@@ -78,8 +74,8 @@ func TestTensorInfo(t *testing.T) {
 			t.Errorf("want shape=%v; got %v (i:%d)", test.Shape, shape, i)
 		}
 
-		if size, want := test.Tensor.Size(), sizeOf(test.Shape); size != want {
-			t.Errorf("want size=%v; got %v (i:%d)", want, size, i)
+		if size := test.Tensor.Size(); size != test.Size {
+			t.Errorf("want size=%v; got %v (i:%d)", test.Size, size, i)
 		}
 
 		if dataSize := len(test.Tensor.Data()); dataSize != test.DataSize {
