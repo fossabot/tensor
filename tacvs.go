32b01908b9700191ac4d921c2ca348532353a5d0
Pawel Knap <pawelknap88@gmail.com>
2018-03-06 11:18:05 +0100

    tensor: add Fill method

--- tacvs.go
+++ tacvs.go
@@ -96,6 +96,20 @@ func (t *Tensor) Size() int {
 	return size
 }
 
+// Fill copies provided buffer to tensor in column-wise order. The size of
+// provided slice must be identical as the size of the tensor.
+func (t *Tensor) Fill(vs []complex128) *Tensor {
+	if len(vs) != len(t.data) {
+		panic("fill with a buffer of invalid size")
+	}
+
+	for i := range t.data {
+		t.data[i] = vs[i]
+	}
+
+	return t
+}
+
 // NDim returns the number of tensor dimensions.
 func (t *Tensor) NDim() int {
 	return len(t.shape)
