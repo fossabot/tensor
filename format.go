1109ee0eb16b46f0c79f325e07e448200bcf3714
Pawel Knap <pawelknap88@gmail.com>
2018-03-21 13:25:52 +0100

    tensor: format add ellipsis to string matrix

--- format.go
+++ format.go
@@ -16,7 +16,28 @@ func (t *Tensor) String() string {
 	return ""
 }
 
-func fmtMatrix(mat [][]complex128) (res [][]string, maxLen int) {
+func addCont(vss [][]string) [][]string {
+	const ellipsis = "⋯"
+
+	for i, vs := range vss {
+		if len(vss[i]) > 1 {
+			vss[i] = append(vs[:len(vs)-1], ellipsis, vs[len(vs)-1])
+		}
+	}
+
+	if len(vss) > 1 {
+		ellVec := make([]string, len(vss[len(vss)-1]))
+		for i := range ellVec {
+			ellVec[i] = ellipsis
+		}
+
+		vss = append(vss[:len(vss)-1], ellVec, vss[len(vss)-1])
+	}
+
+	return vss
+}
+
+func fmtMatrix(mat [][]complex128) (res [][]string) {
 	for _, row := range mat {
 		if len(row) == 0 {
 			continue
@@ -24,15 +45,13 @@ func fmtMatrix(mat [][]complex128) (res [][]string, maxLen int) {
 
 		rows := make([]string, 0, len(row))
 		for _, val := range row {
-			str := fmtCplx(val)
-			maxLen = max(maxLen, len(str))
-			rows = append(rows, str)
+			rows = append(rows, fmtCplx(val))
 		}
 
 		res = append(res, rows)
 	}
 
-	return res, maxLen
+	return res
 }
 
 func fmtCplx(val complex128) string {
@@ -46,12 +65,4 @@ func fmtCplx(val complex128) string {
 	}
 
 	return strings.Trim(fmt.Sprintf("%v", val), "()")
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-
-	return b
 }
