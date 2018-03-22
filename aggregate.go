63bd2a6cca2306dad4a66405e4a1253c824f0575
Pawel Knap <pawelknap88@gmail.com>
2018-03-22 22:31:38 +0100

    tensor: add prototypes for basic aggregate methods

--- aggregate.go
+++ aggregate.go
@@ -16,3 +16,40 @@ func (t *Tensor) Sum() (sum complex128) {
 
 	return sum
 }
+
+// Min returns minimum value of all tensor elements. It returns 0 when the
+// tensor is empty.
+func (t *Tensor) Min() (min complex128) {
+	return
+}
+
+// Max returns maximum value of all tensor elements. It returns 0 when the
+// tensor is empty.
+func (t *Tensor) Max() (max complex128) {
+	return
+}
+
+// CumSum returns a cumulative sum of all tensor elements.
+func (t *Tensor) CumSum() (sum complex128) {
+	return
+}
+
+// Mean returns a mean of all tensor elements.
+func (t *Tensor) Mean() (mean complex128) {
+	return
+}
+
+// Median returns 50th percentile of all tensor elements.
+func (t *Tensor) Median() (median complex128) {
+	return
+}
+
+// CorrCoef returns correletion coefficient of all tensor elements.
+func (t *Tensor) CorrCoef() (cc complex128) {
+	return
+}
+
+// Std returns standard deviation of all tensor elements.
+func (t *Tensor) Std() (std complex128) {
+	return
+}
