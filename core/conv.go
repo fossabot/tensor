cbe3a52bd90bd817a2940c9771bbde962e5ee1ca
Pawel Knap <pawelknap88@gmail.com>
2018-03-25 23:12:12 +0200

    core: add setraw function for unsafe value setting

--- core/conv.go
+++ core/conv.go
@@ -1,29 +1,30 @@
 package core
 
 import (
+	"fmt"
 	"unsafe"
 )
 
-func asBool(typ DType, p unsafe.Pointer) bool {
+func asBool(dst *bool, typ DType, p unsafe.Pointer) {
 	switch typ {
 	case Bool:
-		return *(*bool)(p)
+		*dst = *(*bool)(p)
 	case Int64:
-		return *(*int64)(p) != 0
+		*dst = *(*int64)(p) != 0
 	}
 
 	panic("core: unsupported type: " + typ.String())
 }
 
-func asInt64(typ DType, p unsafe.Pointer) int {
+func asInt64(dst *int, typ DType, p unsafe.Pointer) {
 	switch typ {
 	case Bool:
 		if *(*bool)(p) {
-			return 1
+			*dst = 1
 		}
-		return 0
+		*dst = 0
 	case Int64:
-		return int(*(*int64)(p))
+		*dst = int(*(*int64)(p))
 	}
 
 	panic("core: unsupported type: " + typ.String())
@@ -34,7 +35,8 @@ func asDTypeBool(st DType, sv unsafe.Pointer) unsafe.Pointer {
 		return sv
 	}
 
-	v := asBool(st, sv)
+	var v bool
+	asBool(&v, st, sv)
 	return unsafe.Pointer(&v)
 }
 
@@ -43,7 +45,8 @@ func asDTypeInt64(st DType, sv unsafe.Pointer) unsafe.Pointer {
 		return sv
 	}
 
-	v := asInt64(st, sv)
+	var v int
+	asInt64(&v, st, sv)
 	return unsafe.Pointer(&v)
 }
 
@@ -58,10 +61,38 @@ func convert(dt, st DType, sv unsafe.Pointer) unsafe.Pointer {
 	panic("core: unsupported convert destination type: " + dt.String())
 }
 
-func setReflected(typ DType, dst unsafe.Pointer, v interface{}, f func(dst, vp unsafe.Pointer)) {
+// destruct finds data type of provided value. The returned pointer should be
+// used only for read operations.
+func destruct(v interface{}) (DType, unsafe.Pointer) {
+	switch v := v.(type) {
+	case bool:
+		return Bool, unsafe.Pointer(&v)
+	case int64:
+		return Int64, unsafe.Pointer(&v)
+	}
 
+	panic(fmt.Sprintf("core: unsupported type: %T", v))
 }
 
-func setConverted(dtyp, styp DType, dst, src unsafe.Pointer, f func(dst, vp unsafe.Pointer)) {
-
+// setraw sets underlying pointer value from src to dst based on provided data
+// type. The values pointed by both pointers must be of identical type and the
+// size of provided data type must fit the size of underlying pointers type. If
+// any of these requirements is not met, the behavior of this function is
+// undefined.
+func setraw(t DType, dst, src unsafe.Pointer) {
+	switch t.Size() {
+	case 1:
+		*(*uint8)(dst) = *(*uint8)(src)
+	case 2:
+		*(*uint16)(dst) = *(*uint16)(src)
+	case 4:
+		*(*uint32)(dst) = *(*uint32)(src)
+	case 8:
+		*(*uint64)(dst) = *(*uint64)(src)
+	case 16:
+		*(*uint64)(dst) = *(*uint64)(src)
+		*(*uint64)(unsafe.Pointer(uintptr(dst) + 8)) = *(*uint64)(unsafe.Pointer(uintptr(src) + 8))
+	default:
+		panic("core: unsupported data size: " + t.String())
+	}
 }
