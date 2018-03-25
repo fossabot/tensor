d6ebe5fd2270875234e88170f69ea08d5348e644
Pawel Knap <pawelknap88@gmail.com>
2018-03-25 15:13:16 +0200

    core: add convertion rules for bool<->int64

--- core/conv.go
+++ core/conv.go
@@ -4,8 +4,58 @@ import (
 	"unsafe"
 )
 
-func asBoolean(typ DType, p unsafe.Pointer) bool {
-	return false
+func asBool(typ DType, p unsafe.Pointer) bool {
+	switch typ {
+	case Bool:
+		return *(*bool)(p)
+	case Int64:
+		return *(*int64)(p) != 0
+	}
+
+	panic("core: unsupported type: " + typ.String())
+}
+
+func asInt64(typ DType, p unsafe.Pointer) int {
+	switch typ {
+	case Bool:
+		if *(*bool)(p) {
+			return 1
+		}
+		return 0
+	case Int64:
+		return int(*(*int64)(p))
+	}
+
+	panic("core: unsupported type: " + typ.String())
+}
+
+func asDTypeBool(st DType, sv unsafe.Pointer) unsafe.Pointer {
+	if st == Bool {
+		return sv
+	}
+
+	v := asBool(st, sv)
+	return unsafe.Pointer(&v)
+}
+
+func asDTypeInt64(st DType, sv unsafe.Pointer) unsafe.Pointer {
+	if st == Int64 {
+		return sv
+	}
+
+	v := asInt64(st, sv)
+	return unsafe.Pointer(&v)
+}
+
+func convert(dt, st DType, sv unsafe.Pointer) unsafe.Pointer {
+	switch dt {
+	case Bool:
+		return asDTypeBool(st, sv)
+	case Int64:
+		return asDTypeInt64(st, sv)
+	}
+
+	panic("core: unsupported convert destination type: " + dt.String())
 }
 
 func setReflected(typ DType, dst unsafe.Pointer, v interface{}, f func(dst, vp unsafe.Pointer)) {
--- core/dtype.go
+++ core/dtype.go
@@ -1,15 +1,30 @@
 package core
 
 import (
+	"fmt"
 	"unsafe"
 )
 
 type DType uint64
 
 const (
-	Int64 DType = DType(unsafe.Sizeof(int64(0)))<<32 | 1
+	Bool  DType = DType(unsafe.Sizeof(bool(false)))<<32 | 1
+	Int64 DType = DType(unsafe.Sizeof(int64(0)))<<32 | 2
 )
+
+var dTypeNames = map[DType]string{
+	Bool:  "bool",
+	Int64: "int64",
+}
 
 func (dt DType) Size() uintptr {
 	return uintptr(dt >> 32)
+}
+
+func (dt DType) String() string {
+	if name, ok := dTypeNames[dt]; ok {
+		return name
+	}
+
+	return fmt.Sprintf("unknown(%x)", uint64(dt))
 }
