3d0c9cb4c9bf101db986ffa8ef475b4069cd1a38
Pawel Knap <pawelknap88@gmail.com>
2018-03-27 00:31:31 +0200

    core: add benchmark to DType

--- internal/core/bench_test.go
+++ internal/core/bench_test.go
@@ -3,29 +3,39 @@ package core_test
 import (
 	"reflect"
 	"testing"
+	"unsafe"
 
 	"github.com/ppknap/tacvs/internal/core"
 )
 
-func BenchmarkFillSliceInt1(b *testing.B) {
+func BenchmarkFillSliceInt_1(b *testing.B) {
 	benchmarkFillSliceInt(b, make([]int, 1), 700)
 }
-func BenchmarkFillSliceReflect1(b *testing.B) {
+func BenchmarkFillSliceReflect_1(b *testing.B) {
 	benchmarkFillSliceReflect(b, make([]int, 1), int(700))
 }
+func BenchmarkFillSliceDTypeInt_1(b *testing.B) {
+	benchmarkFillSliceDType(b, core.NewBuffer(1).AsType(core.Int64), int64(700))
+}
 
-func BenchmarkFillSliceInt1000(b *testing.B) {
+func BenchmarkFillSliceInt_1000(b *testing.B) {
 	benchmarkFillSliceInt(b, make([]int, 100), 700)
 }
-func BenchmarkFillSliceReflect1000(b *testing.B) {
+func BenchmarkFillSliceReflect_1000(b *testing.B) {
 	benchmarkFillSliceReflect(b, make([]int, 100), int(700))
 }
-
-func BenchmarkFillSliceInt1000000(b *testing.B) {
-	benchmarkFillSliceInt(b, make([]int, 10000), 700)
+func BenchmarkFillSliceDTypeInt_1000(b *testing.B) {
+	benchmarkFillSliceDType(b, core.NewBuffer(100).AsType(core.Int64), int64(700))
 }
-func BenchmarkFillSliceReflect1000000(b *testing.B) {
-	benchmarkFillSliceReflect(b, make([]int, 10000), int(700))
+
+func BenchmarkFillSliceInt_1000000(b *testing.B) {
+	benchmarkFillSliceInt(b, make([]int, 1000000), 700)
+}
+func BenchmarkFillSliceReflect_1000000(b *testing.B) {
+	benchmarkFillSliceReflect(b, make([]int, 1000000), int(700))
+}
+func BenchmarkFillSliceDTypeInt_1000000(b *testing.B) {
+	benchmarkFillSliceDType(b, core.NewBuffer(1000000).AsType(core.Int64), int64(700))
 }
 
 func benchmarkFillSliceInt(b *testing.B, d []int, v int) {
@@ -56,9 +66,14 @@ func benchmarkFillSliceReflect(b *testing.B, d interface{}, v interface{}) {
 }
 
 func benchmarkFillSliceDType(b *testing.B, buf *core.Buffer, v interface{}) {
-	typ, p := core.Destruct(v)
+	typ, src := core.Destruct(v)
+	if typ != buf.DType() {
+		panic("invalid benchmark argument types")
+	}
 
 	for n := 0; n < b.N; n++ {
-		// TODO
+		buf.Iterate(func(_ int, dst unsafe.Pointer) {
+			typ.Setraw(dst, src)
+		})
 	}
 }
