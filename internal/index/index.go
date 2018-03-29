11d0ee57a6f37397ed302294fc0dad79ae0104a6
Pawel Knap <pawelknap88@gmail.com>
2018-03-29 02:41:55 +0200

    index: document methods and types

--- internal/index/index.go
+++ internal/index/index.go
@@ -1,5 +1,6 @@
 package index
 
+// Index represents an N-dimensional view on one dimensional array.
 type Index struct {
 	scheme IdxScheme
 	shape  []int
@@ -7,18 +8,38 @@ type Index struct {
 	offset int
 }
 
+// NewIndex creates a new Index instance. If scheme is nil, the column-major
+// order scheme will be used.
 func NewIndex(shape []int, scheme IdxScheme) *Index {
 	return nil
 }
 
+// NDim returns the number of dimmensions represented by index.
 func (idx *Index) NDim() int { return 0 }
 
+// Size returns the number of elements safely accessible by index.
 func (idx *Index) Size() int { return 0 }
 
+// At returns array index for a given coordinates.
+func (idx *Index) At(pos []int) int {
+	return 0
+}
+
+// Validate checks if provided position is in index shape boundaries.
+func (idx *Index) Validate(pos []int) bool {
+	return false
+}
+
+// Strides returns offsets to step in each dimmension when traversing 1D array.
 func (idx *Index) Strides() []int { return nil } // copy
 
+// Shape returns an array representing dimmension sizes of the index.
 func (idx *Index) Shape() []int { return nil } // copy
 
+// Slice gets a subset of the index indices and creates a new Index instance
+// which will compute a valid array index using new shape coordinates.
 func (idx *Index) Slice(dim, offset int) *Index { return nil } // shalllow copy
 
+// String satisfies fmt.Stringer interface. It returns some basic info about
+// index object.
 func (idx *Index) String() string { return "" }
--- internal/index/scheme.go
+++ internal/index/scheme.go
@@ -1,27 +1,39 @@
 package index
 
+// IdxScheme defines the order of dimmensions in continuos memory.
+type IdxScheme uint
+
 const (
+	// IdxSchemeColMajor represents column-major order where the vertical values
+	// of a matrix lie side by side in continuos 1D array. This is also known as
+	// Fortran order and is used in MatLab data indexing.
 	IdxSchemeColMajor IdxScheme = iota
 )
 
-type IdxScheme uint
-
-type idxSchemeFunc func([]int) []int
-
+// Strides returns an array which contains data offsets on each dimension.
 func (s IdxScheme) Strides(shape []int) []int {
+	if f, ok := schemeFuncs[s]; ok {
+		return f(shape)
+	}
+
+	panic("index: invalid strided indexing scheme")
+}
+
+var schemeFuncs = map[IdxScheme]func([]int) []int{
+	IdxSchemeColMajor: colMajor,
+}
+
+func colMajor(shape []int) []int {
 	return nil
 }
 
-var schemeFuncs = map[IdxScheme]idxSchemeFunc{
-	IdxSchemeColMajor: columnMajor,
-}
-
-func columnMajor(shape []int) []int {
-	return nil
-}
-
+// String returns the name of index ordering scheme.
 func (s IdxScheme) String() string {
-	return ""
+	if name, ok := schemeNames[s]; ok {
+		return name
+	}
+
+	return "unknown"
 }
 
 var schemeNames = map[IdxScheme]string{
