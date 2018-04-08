1d7e74267b29c0eec6a42bd2383e45b92af7e78a
Pawel Knap <pawelknap88@gmail.com>
2018-04-08 17:34:11 +0200

    core: add string parsing functions

---
+++ internal/core/parse.go
@@ -0,0 +1,55 @@
+package core
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// strAsInt converts provided string to integer type.
+func strAsInt(s string) int64 {
+	if n, err := strconv.ParseInt(s, 10, 64); err == nil {
+		return n
+	}
+
+	if f, err := strconv.ParseFloat(s, 64); err == nil {
+		return int64(f)
+	}
+
+	if b, err := strconv.ParseBool(s); err == nil {
+		if b {
+			return 1
+		}
+		return 0
+	}
+
+	panic(fmt.Sprintf("core: cannot convert %q to an integer value", s))
+}
+
+// strAsFloat converts provided string to floating point number type.
+func strAsFloat(s string) float64 {
+	if f, err := strconv.ParseFloat(s, 64); err == nil {
+		return f
+	}
+
+	if b, err := strconv.ParseBool(s); err == nil {
+		if b {
+			return 1.0
+		}
+		return 0.0
+	}
+
+	panic(fmt.Sprintf("core: cannot convert %q to a float value", s))
+}
+
+// strAsBool converts provided string to boolean type.
+func strAsBool(s string) bool {
+	if b, err := strconv.ParseBool(s); err == nil {
+		return b
+	}
+
+	if f, err := strconv.ParseFloat(s, 64); err == nil {
+		return f != 0.0
+	}
+
+	panic(fmt.Sprintf("core: cannot convert %q to a boolean value", s))
+}
