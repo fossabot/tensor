fe304ad31d8296ab7d50103bcf5e06997ea997c2
Pawel Knap <pawelknap88@gmail.com>
2018-04-10 10:26:21 +0200

    core: refactor benchmarks

--- internal/core/bench_test.go
+++ internal/core/bench_test.go
@@ -8,78 +8,35 @@ import (
 	"github.com/ppknap/tacvs/internal/core"
 )
 
-func BenchmarkFillSliceInt_1(b *testing.B) {
-	benchmarkFillSliceInt(b, make([]int, 1), 700)
-}
-func BenchmarkFillSliceDTypeInt_1(b *testing.B) {
-	benchmarkFillSliceDType(b, core.NewBuffer(1).AsType(core.Int), int(700))
-}
-func BenchmarkFillSliceReflect_1(b *testing.B) {
-	benchmarkFillSliceReflect(b, make([]int, 1), int(700))
-}
-func BenchmarkFillInterfaceSliceReflect_1(b *testing.B) {
-	benchmarkFillSliceReflect(b, make([]interface{}, 1), int(700))
-}
+func BenchmarkRawInt_1(b *testing.B)       { bckRawInt(b, 1) }
+func BenchmarkDTypeInt_1(b *testing.B)     { bckDTypeInt(b, 1) }
+func BenchmarkReflectInt_1(b *testing.B)   { bckReflectInt(b, 1) }
+func BenchmarkInterfaceInt_1(b *testing.B) { bckInterfaceInt(b, 1) }
 
-func BenchmarkFillSliceInt_1000(b *testing.B) {
-	benchmarkFillSliceInt(b, make([]int, 100), 700)
-}
-func BenchmarkFillSliceDTypeInt_1000(b *testing.B) {
-	benchmarkFillSliceDType(b, core.NewBuffer(100).AsType(core.Int), int(700))
-}
-func BenchmarkFillSliceReflect_1000(b *testing.B) {
-	benchmarkFillSliceReflect(b, make([]int, 100), int(700))
-}
-func BenchmarkFillInterfaceSliceReflect_1000(b *testing.B) {
-	benchmarkFillSliceReflect(b, make([]interface{}, 100), int(700))
-}
+func BenchmarkRawInt_1k(b *testing.B)       { bckRawInt(b, 1000) }
+func BenchmarkDTypeInt_1k(b *testing.B)     { bckDTypeInt(b, 1000) }
+func BenchmarkReflectInt_1k(b *testing.B)   { bckReflectInt(b, 1000) }
+func BenchmarkInterfaceInt_1k(b *testing.B) { bckInterfaceInt(b, 1000) }
 
-func BenchmarkFillSliceInt_1000000(b *testing.B) {
-	benchmarkFillSliceInt(b, make([]int, 1000000), 700)
-}
-func BenchmarkFillSliceDTypeInt_1000000(b *testing.B) {
-	benchmarkFillSliceDType(b, core.NewBuffer(1000000).AsType(core.Int), int(700))
-}
-func BenchmarkFillSliceReflect_1000000(b *testing.B) {
-	benchmarkFillSliceReflect(b, make([]int, 1000000), int(700))
-}
-func BenchmarkFillInterfaceSliceReflect_1000000(b *testing.B) {
-	benchmarkFillSliceReflect(b, make([]interface{}, 1000000), int(700))
-}
+func BenchmarkRawInt_1M(b *testing.B)       { bckRawInt(b, 1000000) }
+func BenchmarkDTypeInt_1M(b *testing.B)     { bckDTypeInt(b, 1000000) }
+func BenchmarkReflectInt_1M(b *testing.B)   { bckReflectInt(b, 1000000) }
+func BenchmarkInterfaceInt_1M(b *testing.B) { bckInterfaceInt(b, 1000000) }
 
-func benchmarkFillSliceInt(b *testing.B, d []int, v int) {
+func bckRawInt(b *testing.B, size int) {
+	var buf = make([]int, size)
+
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < len(d); i++ {
-			d[i] = (v + i) % v / 2
+		for i := 0; i < size; i++ {
+			buf[i] = (size + i) % size / 2
 		}
 	}
 }
 
-func benchmarkFillSliceReflect(b *testing.B, d interface{}, v interface{}) {
-	if kind := reflect.TypeOf(d).Kind(); kind != reflect.Slice {
-		panic("invalid benchmark data type")
-	}
+func bckDTypeInt(b *testing.B, size int) { bckDType(b, core.NewBuffer(size).AsType(core.Int), size) }
 
-	var setter func()
-
-	s := reflect.ValueOf(d)
-	switch v := v.(type) {
-	case int:
-		setter = func() {
-			for i := 0; i < s.Len(); i++ {
-				s.Index(i).Set(reflect.ValueOf((v + i) % v / 2))
-			}
-		}
-	default:
-		panic("invalid benchmark input value")
-	}
-
-	for n := 0; n < b.N; n++ {
-		setter()
-	}
-}
-
-func benchmarkFillSliceDType(b *testing.B, buf *core.Buffer, v interface{}) {
+func bckDType(b *testing.B, buf *core.Buffer, v interface{}) {
 	dt, src := core.Destruct(v)
 	if dt != buf.DType() {
 		panic("invalid benchmark argument types")
@@ -95,9 +52,36 @@ func benchmarkFillSliceDType(b *testing.B, buf *core.Buffer, v interface{}) {
 		panic("core: unsupported type: " + dt.String())
 	}
 
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		buf.Iterate(func(i int, dst unsafe.Pointer) {
 			setter(i, dst, src)
 		})
+	}
+}
+
+func bckReflectInt(b *testing.B, size int)   { bckReflect(b, make([]int, size), size) }
+func bckInterfaceInt(b *testing.B, size int) { bckReflect(b, make([]interface{}, size), size) }
+
+func bckReflect(b *testing.B, d interface{}, v interface{}) {
+	if kind := reflect.TypeOf(d).Kind(); kind != reflect.Slice {
+		panic("invalid benchmark data type")
+	}
+
+	var setter = func() { panic("invalid benchmark input") }
+
+	s := reflect.ValueOf(d)
+	switch v := v.(type) {
+	case int:
+		setter = func() {
+			for i := 0; i < s.Len(); i++ {
+				s.Index(i).Set(reflect.ValueOf((v + i) % v / 2))
+			}
+		}
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		setter()
 	}
 }
