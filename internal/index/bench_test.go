6ff564195edb8b1d78c097a773310bf8c402bbe9
Pawel Knap <pawelknap88@gmail.com>
2018-04-10 09:58:26 +0200

    index: add benchmarks for index iterator

---
+++ internal/index/bench_test.go
@@ -0,0 +1,50 @@
+package index_test
+
+import (
+	"testing"
+
+	"github.com/ppknap/tacvs/internal/index"
+)
+
+func BenchmarkIterateRaw_1D_1E(b *testing.B)  { bckIterateRaw(b, []int{1}) }
+func BenchmarkIterateAt_1D_1E(b *testing.B)   { bckIterateAt(b, []int{1}) }
+func BenchmarkIterateRaw_1D_1kE(b *testing.B) { bckIterateRaw(b, []int{1000}) }
+func BenchmarkIterateAt_1D_1kE(b *testing.B)  { bckIterateAt(b, []int{1000}) }
+func BenchmarkIterateRaw_1D_1ME(b *testing.B) { bckIterateRaw(b, []int{1000000}) }
+func BenchmarkIterateAt_1D_1ME(b *testing.B)  { bckIterateAt(b, []int{1000000}) }
+
+func BenchmarkIterateRaw_2D_1E(b *testing.B)   { bckIterateRaw(b, []int{1, 1}) }
+func BenchmarkIterateAt_2D_1E(b *testing.B)    { bckIterateAt(b, []int{1, 1}) }
+func BenchmarkIterateRaw_2D_100E(b *testing.B) { bckIterateRaw(b, []int{100, 100}) }
+func BenchmarkIterateAt_2D_100E(b *testing.B)  { bckIterateAt(b, []int{100, 100}) }
+func BenchmarkIterateRaw_2D_1kE(b *testing.B)  { bckIterateRaw(b, []int{1000, 1000}) }
+func BenchmarkIterateAt_2D_1kE(b *testing.B)   { bckIterateAt(b, []int{1000, 1000}) }
+
+func BenchmarkIterateRaw_3D_1E(b *testing.B)   { bckIterateRaw(b, []int{1, 1, 1}) }
+func BenchmarkIterateAt_3D_1E(b *testing.B)    { bckIterateAt(b, []int{1, 1, 1}) }
+func BenchmarkIterateRaw_3D_10E(b *testing.B)  { bckIterateRaw(b, []int{10, 10, 10}) }
+func BenchmarkIterateAt_3D_10E(b *testing.B)   { bckIterateAt(b, []int{10, 10, 10}) }
+func BenchmarkIterateRaw_3D_100E(b *testing.B) { bckIterateRaw(b, []int{100, 100, 100}) }
+func BenchmarkIterateAt_3D_100E(b *testing.B)  { bckIterateAt(b, []int{100, 100, 100}) }
+
+func bckIterateRaw(b *testing.B, shape []int) {
+	idx, sum := index.NewIndex(shape, index.IdxSchemeColMajor), 0
+	for n := 0; n < b.N; n++ {
+		idx.Iterate(func(pos []int) {
+			sum = len(pos)
+		})
+
+	}
+	_ = sum
+}
+
+func bckIterateAt(b *testing.B, shape []int) {
+	idx, sum := index.NewIndex(shape, index.IdxSchemeColMajor), 0
+	for n := 0; n < b.N; n++ {
+		idx.Iterate(func(pos []int) {
+			sum = idx.At(pos)
+		})
+
+	}
+	_ = sum
+}
