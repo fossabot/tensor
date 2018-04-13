44abf0c129ef72bf793c361325d82c515297ed1c
Pawel Knap <pawelknap88@gmail.com>
2018-04-13 23:07:16 +0200

    tests: add draft of test generator

--- tensor/cmd/tensor/main.go
+++ tensor/cmd/tensor/main.go
@@ -3,35 +3,81 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
+	"text/template"
 )
 
+var instances = []struct {
+	Name string // Tensor name.
+	GoT  string // Go tensor initialization.
+	PyA  string // Python's ndarray initialization.
+}{
+	{Name: "matrix one element", GoT: "New(1, 1)", PyA: "zeros((1, 1))"},
+}
+
+var methods = map[string][]struct {
+	Name  string            // Method name.
+	RTyp  string            // Function return type.
+	Cases map[string]string // Go to python method call.
+}{
+	"layout": {{
+		Name: "NDim",
+		RTyp: "int",
+		Cases: map[string]string{
+			"NDim()": "ndim",
+		},
+	}, {
+		Name: "Size",
+		RTyp: "int",
+		Cases: map[string]string{
+			"Size()": "size",
+		},
+	}},
+}
+
+// TestCase describes a single test case.
+type TestCase struct {
+	Name   string // Subtest name.
+	Tensor string // Tensor built from instances.
+	Expr   string // Go expression to call.
+	Want   string // Wanted result.
+}
+
+// TestFull describes all tests for a single method.
+type TestFull struct {
+	Name  string // Tested method.
+	RTyp  string
+	Pass  []TestCase // Test cases that should pass.
+	Panic []TestCase // Test cases that should panic.
+}
+
+func generateTests(group string) (res []TestFull) {
+	methods, ok := methods[group]
+	if !ok {
+		panic("unknown test group: " + group)
+	}
+
+	for i, method := range methods {
+		tf := TestFull{Name: method.Name}
+
+	}
+
+	return res
+}
+
 func main() {
-	generator()
-	fmt.Println(">>", runPyCmd("version.version"))
+	for _, methods := range methods {
+		var data = struct{ Is, Ms interface{} }{
+			Is: instances, Ms: methods,
+		}
+
+		fmt.Println(tmpl.Execute(os.Stdout, data))
+	}
 }
 
 func generator() {
-	instances := []struct {
-		GoT string // Go tensor initialization.
-		PyA string // Python's ndarray initialization.
-	}{
-		{
-			GoT: "NewTensor(1, 1)", PyA: "zeros((1, 1))",
-		},
-	}
-
-	methods := map[string]struct {
-		Name string                     // Test name.
-		GoM  string                     // Go method to call.
-		PyM  string                     // Python method to call.
-		Cmp  func(output string) string // How to compare outputs.
-	}{
-		"layout": {
-			GoM: "NDim()", PyM: "ndim", Cmp: nil,
-		},
-	}
 
 	_ = instances
 	_ = methods
@@ -53,7 +99,7 @@ func runPyCmd(op string) string {
 		panic(fmt.Sprintf("cannot run %q: %v (output %q)", code, err, out))
 	}
 
-	return string(out)
+	return string(bytes.TrimSpace(out))
 }
 
 func cmpInt(output string) string {
@@ -62,3 +108,30 @@ func cmpInt(output string) string {
 	}
 	panic("invalid integer: " + output)
 }
+
+var tmpl = template.Must(template.New("").Parse(`// TODO
+
+package tensor_test
+
+import (
+	"testing"
+)
+
+{{ range .Ms }}
+func TestTensor{{ .Name }}(t *testing.T) {
+	tests := map[string]struct {
+		Tensor *tensor.Tensor
+		Want   {{ .RTyp }}
+	}{ {{- range $.Is }}
+		"{{ .Name }}": {
+			Tensor: {{ .GoT }}
+			Want:
+		} {{ end }}
+	}
+
+	for name, test := range tests {
+		if got := test.{{ .GoM }};
+	}
+}
+{{ end }}
+`))
