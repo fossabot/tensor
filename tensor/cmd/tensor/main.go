a9cb0d5eb84d8ca2d9afee8939bbefd63f806eb9
Pawel Knap <pawelknap88@gmail.com>
2018-04-15 21:56:30 +0200

    tests: generate test cases initial version

--- tensor/cmd/tensor/main.go
+++ tensor/cmd/tensor/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strconv"
@@ -20,84 +21,121 @@ func init() {
 	case err != nil:
 		panic(err)
 	case strings.HasPrefix(ver, numpyVer):
-		fmt.Printf("Numpy %s will be used to generate test cases.\n", ver)
+		log.Printf("numpy %s will be used to generate test cases.\n", ver)
 	default:
 		panic(fmt.Sprintf("invalid numpy version %s (want: %s)", ver, numpyVer))
 	}
 }
 
 var instances = []struct {
-	Name string // Tensor name.
-	GoT  string // Go tensor initialization.
-	PyA  string // Python's ndarray initialization.
+	Name    string // Tensor name.
+	Tensor  string // Go tensor initialization.
+	NDArray string // Python's ndarray initialization.
 }{
-	{Name: "matrix one element", GoT: "New(1, 1)", PyA: "zeros((1, 1))"},
+	{Name: "matrix one element", Tensor: "New(1, 1)", NDArray: "zeros((1, 1))"},
 }
 
 var methods = map[string][]struct {
-	Name  string            // Method name.
-	RTyp  string            // Function return type.
-	Cases map[string]string // Go to python method call.
+	Name  string // Method name.
+	RTyp  string // Function return type.
+	Calls []Call // Go to python method call.
 }{
 	"layout": {{
 		Name: "NDim",
 		RTyp: "int",
-		Cases: map[string]string{
-			"NDim()": "ndim",
+		Calls: []Call{
+			{
+				Go: "NDim()",
+				Py: "ndim",
+			},
 		},
 	}, {
 		Name: "Size",
 		RTyp: "int",
-		Cases: map[string]string{
-			"Size()": "size",
+		Calls: []Call{
+			{
+				Go: "Size()",
+				Py: "size",
+			},
 		},
 	}},
 }
 
+// Call describes corresponding Go and Python method calls.
+type Call struct {
+	Desc string // Additional description.
+	Go   string // Call in Go.
+	Py   string // Call in Python.
+}
+
 // TestCase describes a single test case.
 type TestCase struct {
-	Name   string // Subtest name.
-	Tensor string // Tensor built from instances.
-	Expr   string // Go expression to call.
-	Want   string // Wanted result.
+	Name string // Subtest name.
+	Expr string // Go expression to call.
+	Want string // Wanted result.
 }
 
 // TestFull describes all tests for a single method.
 type TestFull struct {
-	Name  string // Tested method.
-	RTyp  string
-	Pass  []TestCase // Test cases that should pass.
-	Panic []TestCase // Test cases that should panic.
+	Name  string      // Tested method.
+	RTyp  string      // Output type.
+	Pass  []*TestCase // Test cases that should pass.
+	Panic []*TestCase // Test cases that should panic.
 }
 
 func generateTests(group string) (res []TestFull) {
-	// methods, ok := methods[group]
-	// if !ok {
-	// 	panic("unknown test group: " + group)
-	// }
+	methods, ok := methods[group]
+	if !ok {
+		panic("unknown test group: " + group)
+	}
 
-	// for i, method := range methods {
-	// 	tf := TestFull{Name: method.Name}
+	for _, method := range methods {
+		var tf = TestFull{
+			Name: method.Name,
+			RTyp: method.RTyp,
+		}
 
-	// }
+		for _, call := range method.Calls {
+			for _, inst := range instances {
+				var expr = inst.NDArray + "." + call.Py
+
+				output, err := runPyCmd(expr)
+				if err != nil {
+					log.Printf("cannot execute %q: %v", expr, err)
+					continue
+				}
+
+				if _, ok := typeToExpr[method.RTyp]; !ok {
+					panic("unknown type: " + method.RTyp)
+				}
+
+				tc := &TestCase{
+					Name: strings.TrimSpace(inst.Name + " " + call.Desc),
+					Expr: inst.Tensor + "." + call.Go,
+					Want: typeToExpr[method.RTyp](output),
+				}
+
+				if output != "" {
+					tf.Pass = append(tf.Pass, tc)
+					log.Printf("generated tests for %q, output %q: OK", tc.Expr, tc.Want)
+				} else {
+					tf.Panic = append(tf.Panic, tc)
+					log.Printf("generated tests for %q: PANIC", tc.Expr)
+				}
+			}
+		}
+
+		res = append(res, tf)
+	}
 
 	return res
 }
 
 func main() {
-	for _, methods := range methods {
-		var data = struct{ Is, Ms interface{} }{
-			Is: instances, Ms: methods,
-		}
-
-		fmt.Println(tmpl.Execute(os.Stdout, data))
+	tests := generateTests("layout")
+	if err := tmpl.Execute(os.Stdout, tests); err != nil {
+		panic("template error: " + err.Error())
 	}
-}
-
-func generator() {
-
-	_ = instances
-	_ = methods
 }
 
 func runPyCmd(op string) (string, error) {
@@ -119,6 +157,21 @@ func runPyCmd(op string) (string, error) {
 	return string(bytes.TrimSpace(out)), nil
 }
 
+// typeToExpr i a set of functions that format and validate given output to
+// Go expression based on expected type.
+var typeToExpr = map[string]func(string) string{
+	"int": skipEmpty(cmpInt),
+}
+
+func skipEmpty(f func(string) string) func(string) string {
+	return func(output string) string {
+		if output == "" {
+			return ""
+		}
+		return f(output)
+	}
+}
+
 func cmpInt(output string) string {
 	if _, err := strconv.Atoi(output); err == nil {
 		return fmt.Sprintf("int(%s)", output)
@@ -134,20 +187,22 @@ import (
 	"testing"
 )
 
-{{ range .Ms }}
+{{ range . }}
 func TestTensor{{ .Name }}(t *testing.T) {
 	tests := map[string]struct {
-		Tensor *tensor.Tensor
-		Want   {{ .RTyp }}
-	}{ {{- range $.Is }}
+		Got  {{ .RTyp }}
+		Want {{ .RTyp }}
+	}{ {{- range .Pass }}
 		"{{ .Name }}": {
-			Tensor: {{ .GoT }}
-			Want:
+			Tensor: {{ .Expr }},
+			Want: {{ .Want }},
 		} {{ end }}
 	}
 
 	for name, test := range tests {
-		if got := test.{{ .GoM }};
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want %v; got %v", want, got)
+		}
 	}
 }
 {{ end }}
