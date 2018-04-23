9e01c01beae54d596ca2a6682b2abf11a1051594
Pawel Knap <pawelknap88@gmail.com>
2018-04-23 17:43:41 +0200

    tensor: clean tensor.go file

--- tensor.go
+++ tensor.go
@@ -1,14 +1,11 @@
 package tensor
 
 import (
-	"fmt"
-
-	"github.com/ppknap/tensor/dtype"
 	"github.com/ppknap/tensor/internal/core"
 	"github.com/ppknap/tensor/internal/index"
-	"github.com/ppknap/tensor/internal/math"
 )
 
+// Tensor represents an organized multidimensional array of fixed-size elements.
 type Tensor struct {
 	idx *index.Index
 	buf *core.Buffer
@@ -38,79 +35,8 @@ func NewScalar(scalar interface{}) *Tensor {
 	return t
 }
 
-func (t *Tensor) At(pos ...int) *Tensor {
-	if !t.idx.Validate(pos) {
-		panic(fmt.Sprintf("tensor: invalid position %v for %v", pos, t.idx))
-	}
-
-	return &Tensor{
-		idx: t.idx.Scalar(pos),
-		buf: t.buf,
-	}
-}
-
-// Each TODO.
-func Each(f func(t *Tensor)) *Tensor {
-	return nil
-}
-
-func (t *Tensor) View() *Tensor {
-	if t.idx != nil && t.idx.Flags().IsView() {
-		return t
-	}
-
-	return &Tensor{
-		idx: t.idx.View(),
-		buf: t.buf,
-	}
-}
-
-func (t *Tensor) Delegate() *Delegate {
-	return NewDelegate(t)
-}
-
-// Reshape TODO.
-func (t *Tensor) Reshape(shape ...int) *Tensor {
-	return nil
-}
-
-// AsType TODO.
-func (t *Tensor) AsType(typ dtype.DType) *Tensor {
-	return t
-}
-
 func (t *Tensor) init() {
 	if t.idx == nil && t.buf == nil {
 		*t = *New()
 	}
 }
-
-type Delegate struct {
-	dst *Tensor
-}
-
-func NewDelegate(dst *Tensor) *Delegate {
-	return &Delegate{dst: dst}
-}
-
-func (d *Delegate) Add(a, b *Tensor) (dst *Tensor) {
-	if a == nil || b == nil {
-		panic(core.NewError("nil argument provided"))
-	}
-
-	var shape = math.EWArgShape(a.idx, b.idx)
-
-	if dst = d.dst; dst == nil {
-		dst = New(shape...).AsType(core.Promote(a.DType(), b.DType()))
-	} else if ds := dst.Shape(); !index.EqShape(ds, shape) {
-		panic(core.NewError("invalid dst shape %v for %v", ds, shape))
-	}
-
-	math.Binary(dst.idx, a.idx, b.idx, dst.buf, a.buf, b.buf, math.Add)
-
-	return dst
-}
-
-// View
-// Scheme
-// Size(l) && Size(r) || Size(l) && Size(r) == 1
