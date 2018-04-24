017a919a004ab46d49bf565e3fa052d013a2f3ed
Pawel Knap <pawelknap88@gmail.com>
2018-04-24 21:36:45 +0200

    tensor: add prototypes for conversion methods

--- conv.go
+++ conv.go
@@ -1,5 +1,12 @@
 package tensor
 
+import "github.com/ppknap/tensor/dtype"
+
+// Copy TODO.
+func (t *Tensor) Copy() *Tensor {
+	return nil
+}
+
 // View TODO.
 func (t *Tensor) View() *Tensor {
 	if t.idx != nil && t.idx.Flags().IsView() {
@@ -10,4 +17,39 @@ func (t *Tensor) View() *Tensor {
 		idx: t.idx.View(),
 		buf: t.buf,
 	}
+}
+
+// AsType TODO.
+func (t *Tensor) AsType(dt dtype.DType) *Tensor {
+	return nil
+}
+
+// Bool TODO.
+func (t *Tensor) Bool() bool {
+	return false
+}
+
+// Byte TODO.
+func (t *Tensor) Byte() byte {
+	return 0
+}
+
+// Int TODO.
+func (t *Tensor) Int() int {
+	return 0
+}
+
+// Float TODO.
+func (t *Tensor) Float() float64 {
+	return 0
+}
+
+// Cmplx TODO.
+func (t *Tensor) Cmplx() complex128 {
+	return 0
+}
+
+// Object TODO.
+func (t *Tensor) Object() interface{} {
+	return nil
 }
