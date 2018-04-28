c103b95f036345ce266049036805841e130e69bc
Pawel Knap <pawelknap88@gmail.com>
2018-04-28 16:59:25 +0200

    core: add subtract operation

--- internal/math/subtract.go
+++ internal/math/subtract.go
@@ -6,77 +6,75 @@ import (
 	"github.com/ppknap/tensor/internal/core"
 )
 
-// Add is a binary function responsible for addition.
-func Add(dt core.DType) BinaryFunc {
+// Subtract is a binary function responsible for subtraction.
+func Subtract(dt core.DType) BinaryFunc {
 	switch dt {
 	case core.Bool:
 		return func(d, l, r unsafe.Pointer) {
-			*(*bool)(d) = *(*bool)(l) || *(*bool)(r)
+			*(*bool)(d) = *(*bool)(l) != *(*bool)(r) // Logical XOR.
 		}
 	case core.Int:
 		return func(d, l, r unsafe.Pointer) {
-			*(*int)(d) = *(*int)(l) + *(*int)(r)
+			*(*int)(d) = *(*int)(l) - *(*int)(r)
 		}
 	case core.Int8:
 		return func(d, l, r unsafe.Pointer) {
-			*(*int8)(d) = *(*int8)(l) + *(*int8)(r)
+			*(*int8)(d) = *(*int8)(l) - *(*int8)(r)
 		}
 	case core.Int16:
 		return func(d, l, r unsafe.Pointer) {
-			*(*int16)(d) = *(*int16)(l) + *(*int16)(r)
+			*(*int16)(d) = *(*int16)(l) - *(*int16)(r)
 		}
 	case core.Int32:
 		return func(d, l, r unsafe.Pointer) {
-			*(*int32)(d) = *(*int32)(l) + *(*int32)(r)
+			*(*int32)(d) = *(*int32)(l) - *(*int32)(r)
 		}
 	case core.Int64:
 		return func(d, l, r unsafe.Pointer) {
-			*(*int64)(d) = *(*int64)(l) + *(*int64)(r)
+			*(*int64)(d) = *(*int64)(l) - *(*int64)(r)
 		}
 	case core.Uint:
 		return func(d, l, r unsafe.Pointer) {
-			*(*uint)(d) = *(*uint)(l) + *(*uint)(r)
+			*(*uint)(d) = *(*uint)(l) - *(*uint)(r)
 		}
 	case core.Uint8:
 		return func(d, l, r unsafe.Pointer) {
-			*(*uint8)(d) = *(*uint8)(l) + *(*uint8)(r)
+			*(*uint8)(d) = *(*uint8)(l) - *(*uint8)(r)
 		}
 	case core.Uint16:
 		return func(d, l, r unsafe.Pointer) {
-			*(*uint16)(d) = *(*uint16)(l) + *(*uint16)(r)
+			*(*uint16)(d) = *(*uint16)(l) - *(*uint16)(r)
 		}
 	case core.Uint32:
 		return func(d, l, r unsafe.Pointer) {
-			*(*uint32)(d) = *(*uint32)(l) + *(*uint32)(r)
+			*(*uint32)(d) = *(*uint32)(l) - *(*uint32)(r)
 		}
 	case core.Uint64:
 		return func(d, l, r unsafe.Pointer) {
-			*(*uint64)(d) = *(*uint64)(l) + *(*uint64)(r)
+			*(*uint64)(d) = *(*uint64)(l) - *(*uint64)(r)
 		}
 	case core.Uintptr:
 		return func(d, l, r unsafe.Pointer) {
-			*(*uintptr)(d) = *(*uintptr)(l) + *(*uintptr)(r)
+			*(*uintptr)(d) = *(*uintptr)(l) - *(*uintptr)(r)
 		}
 	case core.Float32:
 		return func(d, l, r unsafe.Pointer) {
-			*(*float32)(d) = *(*float32)(l) + *(*float32)(r)
+			*(*float32)(d) = *(*float32)(l) - *(*float32)(r)
 		}
 	case core.Float64:
 		return func(d, l, r unsafe.Pointer) {
-			*(*float64)(d) = *(*float64)(l) + *(*float64)(r)
+			*(*float64)(d) = *(*float64)(l) - *(*float64)(r)
 		}
 	case core.Complex64:
 		return func(d, l, r unsafe.Pointer) {
-			*(*complex64)(d) = *(*complex64)(l) + *(*complex64)(r)
+			*(*complex64)(d) = *(*complex64)(l) - *(*complex64)(r)
 		}
 	case core.Complex128:
 		return func(d, l, r unsafe.Pointer) {
-			*(*complex128)(d) = *(*complex128)(l) + *(*complex128)(r)
+			*(*complex128)(d) = *(*complex128)(l) - *(*complex128)(r)
 		}
 	case core.String:
-		return func(d, l, r unsafe.Pointer) {
-			*(*string)(d) = *(*string)(l) + *(*string)(r)
-		}
+		panic(core.NewError("invalid subtraction of strings"))
 	}
 
 	panic(core.NewError("unsupported type: %q", dt))
