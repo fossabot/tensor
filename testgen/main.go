f9b155371b40cd4d09903ce5ecee6500ebe7970d
Pawel Knap <pawelknap88@gmail.com>
2018-05-02 00:36:36 +0200

    tests: generate tests for multiple files at a time

--- testgen/main.go
+++ testgen/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -44,33 +46,54 @@ func main() {
 		log.Fatalf("cannot get methods directory: %v", err)
 	}
 
-	var nAllPass, nAllPanic int
-	for _, m := range match {
-		name := strings.TrimSuffix(filepath.Base(m), ".json")
-		outFilepath := filepath.Join(*outPath, name+"_test.go")
+	const parallelFilesN = 4
+	ticketC := make(chan struct{}, parallelFilesN)
 
-		mts, err := loadMethods(m)
-		if err != nil {
-			log.Fatalf("cannot load methods from %q: %v", m, err)
-		}
-
-		nPass, nPanic, err := generate(outFilepath, mts, ins)
-		if err != nil {
-			log.Fatalf("cannot generate tests from %v: %v", m, err)
-		}
-
-		nAllPass += nPass
-		nAllPanic += nPanic
-
-		log.Printf("generated tests from %v were saved in %v", m, outFilepath)
+	for i := 0; i < parallelFilesN; i++ {
+		ticketC <- struct{}{}
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(match))
+
+	var nAllPass, nAllPanic int64
+	for _, m := range match {
+		m := m // Capture range variables.
+		go func() {
+			defer wg.Done()
+
+			<-ticketC
+
+			name := strings.TrimSuffix(filepath.Base(m), ".json")
+			outFilepath := filepath.Join(*outPath, name+"_test.go")
+
+			mts, err := loadMethods(m)
+			if err != nil {
+				log.Fatalf("cannot load methods from %q: %v", m, err)
+			}
+
+			nPass, nPanic, err := generate(outFilepath, mts, ins)
+			if err != nil {
+				log.Fatalf("cannot generate tests from %v: %v", m, err)
+			}
+
+			ticketC <- struct{}{}
+
+			atomic.AddInt64(&nAllPass, nPass)
+			atomic.AddInt64(&nAllPanic, nPanic)
+
+			log.Printf("generated tests from %v were saved in %v", m, outFilepath)
+		}()
+	}
+
+	wg.Wait()
 
 	log.Println("done!")
 	log.Printf("generated %d tests in %v, %d of them are panic handlers",
 		nAllPass+nAllPanic, time.Since(now), nAllPanic)
 }
 
-func generate(outFilepath string, mts []*method, ins []*instance) (nPass, nPanic int, err error) {
+func generate(outFilepath string, mts []*method, ins []*instance) (nPass, nPanic int64, err error) {
 	type idxTest struct {
 		i int
 		t *test
@@ -91,8 +114,8 @@ func generate(outFilepath string, mts []*method, ins []*instance) (nPass, nPanic
 	for i := 0; i < len(mts); i++ {
 		test := <-testC
 		tests[test.i] = test.t
-		nPass += len(test.t.Pass)
-		nPanic += len(test.t.Panic)
+		nPass += int64(len(test.t.Pass))
+		nPanic += int64(len(test.t.Panic))
 	}
 
 	close(testC)
