b7fdeec15ccef11ff18aa88790304afbe0fbe53b
Pawel Knap <pawelknap88@gmail.com>
2018-05-07 12:42:33 +0200

    routine: add Re implementation

---
+++ internal/routine/re.go
@@ -0,0 +1,31 @@
+package routine
+
+import (
+	"unsafe"
+
+	"github.com/ppknap/tensor/internal/dtype"
+	"github.com/ppknap/tensor/internal/errorc"
+	"github.com/ppknap/tensor/internal/math"
+)
+
+// Re is a nullary function that removes imaginary part from called object.
+func Re(dt dtype.DType) math.NullaryFunc {
+	switch dt {
+	case dtype.Bool, dtype.Int, dtype.Int8, dtype.Int16, dtype.Int32, dtype.Int64,
+		dtype.Uint, dtype.Uint8, dtype.Uint16, dtype.Uint32, dtype.Uint64,
+		dtype.Uintptr, dtype.Float32, dtype.Float64:
+		return func(_ []int, d unsafe.Pointer) { /* no-op */ }
+	case dtype.Complex64:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*complex64)(d) = complex(real(*(*complex64)(d)), 0)
+		}
+	case dtype.Complex128:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*complex128)(d) = complex(real(*(*complex128)(d)), 0)
+		}
+	case dtype.String:
+		panic(errorc.New("invalid real part of string type"))
+	}
+
+	panic(errorc.New("unsupported type: %q", dt))
+}
