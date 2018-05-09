8a238b2cf141fd6b5f2f26b023c56804c88f0bb7
Pawel Knap <pawelknap88@gmail.com>
2018-05-10 00:04:00 +0200

    routine: add support for integers in Exp implementation

--- internal/routine/exp.go
+++ internal/routine/exp.go
@@ -13,18 +13,58 @@ import (
 // Exp is a nullary function that computes exponential of a called object.
 func Exp(dt dtype.DType) math.NullaryFunc {
 	switch dt {
-	case dtype.Bool, dtype.Int, dtype.Int8, dtype.Int16, dtype.Int32, dtype.Int64,
-		dtype.Uint, dtype.Uint8, dtype.Uint16, dtype.Uint32, dtype.Uint64,
-		dtype.Uintptr, dtype.String:
+	case dtype.Bool, dtype.String:
+		panic(errorc.New("invalid exponential on %s type", dt))
+	case dtype.Int:
 		return func(_ []int, d unsafe.Pointer) {
-			panic(errorc.New("invalid exponential on %s type", dt))
+			*(*int)(d) = int(stdmath.Round(stdmath.Exp(float64(*(*int)(d)))))
+		}
+	case dtype.Int8:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*int8)(d) = int8(stdmath.Round(stdmath.Exp(float64(*(*int8)(d)))))
+		}
+	case dtype.Int16:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*int16)(d) = int16(stdmath.Round(stdmath.Exp(float64(*(*int16)(d)))))
+		}
+	case dtype.Int32:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*int32)(d) = int32(stdmath.Round(stdmath.Exp(float64(*(*int32)(d)))))
+		}
+	case dtype.Int64:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*int64)(d) = int64(stdmath.Round(stdmath.Exp(float64(*(*int64)(d)))))
+		}
+	case dtype.Uint:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*uint)(d) = uint(stdmath.Round(stdmath.Exp(float64(*(*uint)(d)))))
+		}
+	case dtype.Uint8:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*uint8)(d) = uint8(stdmath.Round(stdmath.Exp(float64(*(*uint8)(d)))))
+		}
+	case dtype.Uint16:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*uint16)(d) = uint16(stdmath.Round(stdmath.Exp(float64(*(*uint16)(d)))))
+		}
+	case dtype.Uint32:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*uint32)(d) = uint32(stdmath.Round(stdmath.Exp(float64(*(*uint32)(d)))))
+		}
+	case dtype.Uint64:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*uint64)(d) = uint64(stdmath.Round(stdmath.Exp(float64(*(*uint64)(d)))))
+		}
+	case dtype.Uintptr:
+		return func(_ []int, d unsafe.Pointer) {
+			*(*uintptr)(d) = uintptr(stdmath.Round(stdmath.Exp(float64(*(*uintptr)(d)))))
 		}
 	case dtype.Float32:
-		return func(pos []int, d unsafe.Pointer) {
+		return func(_ []int, d unsafe.Pointer) {
 			*(*float32)(d) = float32(stdmath.Exp(float64(*(*float32)(d))))
 		}
 	case dtype.Float64:
-		return func(pos []int, d unsafe.Pointer) {
+		return func(_ []int, d unsafe.Pointer) {
 			*(*float64)(d) = stdmath.Exp(*(*float64)(d))
 		}
 	case dtype.Complex64:
