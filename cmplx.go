c01b5a8d941637ac79b9b0b8a0d36ecb74b19aa2
Pawel Knap <pawelknap88@gmail.com>
2018-05-20 11:28:57 +0200

    tensor: implement and test Real and Imag methods

--- cmplx.go
+++ cmplx.go
@@ -1,11 +1,26 @@
 package tensor
 
-// Real TODO.
+import (
+	"github.com/ppknap/tensor/internal/math"
+	"github.com/ppknap/tensor/internal/routine"
+)
+
+// Real removes imaginary part from all elements. This method is a no-op for all
+// non-complex numerical elements.
 func (t *Tensor) Real() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Real)
+
 	return t
 }
 
-// Imag TODO.
+// Imag removes real part from all elements. Non complex numerical elements will
+// be set to their zero values.
 func (t *Tensor) Imag() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Imag)
+
 	return t
 }
