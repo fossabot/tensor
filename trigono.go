f2b3e11b502322d76830d50193946125163eca31
Pawel Knap <pawelknap88@gmail.com>
2018-05-20 12:02:40 +0200

    tensor: implement and test Sin, Cos and Tan methods

--- trigono.go
+++ trigono.go
@@ -1,16 +1,33 @@
 package tensor
 
-// Sin TODO.
+import (
+	"github.com/ppknap/tensor/internal/math"
+	"github.com/ppknap/tensor/internal/routine"
+)
+
+// Sin computes trigonometric sine on all radian elements.
 func (t *Tensor) Sin() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Sin)
+
 	return t
 }
 
-// Cos TODO.
+// Cos computes trigonometric cosine on all radian elements.
 func (t *Tensor) Cos() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Cos)
+
 	return t
 }
 
-// Tan TODO.
+// Tan computes trigonometric tangent on all radian elements.
 func (t *Tensor) Tan() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Tan)
+
 	return t
 }
