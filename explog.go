523d7c0a0dab7ad2f3b3254c67254ad65e66cc54
Pawel Knap <pawelknap88@gmail.com>
2018-05-21 15:28:26 +0200

    tensor: implement and test Exp, Pow, Sqrt, Log and Log10 methods

--- explog.go
+++ explog.go
@@ -1,26 +1,55 @@
 package tensor
 
-// Exp TODO.
+import (
+	"github.com/ppknap/tensor/internal/math"
+	"github.com/ppknap/tensor/internal/routine"
+)
+
+// Exp computes e**x of all tensor elements.
 func (t *Tensor) Exp() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Exp)
+
 	return t
 }
 
-// Pow TODO.
+// Pow rises each tensor element to the coresponding value of provided argument.
 func (t *Tensor) Pow(v *Tensor) *Tensor {
+	t.init()
+	v.init()
+
+	// Check if v is assignable to t.
+	_ = math.ElementWiseDstShape(t.idx, v.idx, false)
+
+	math.Unary(t.idx, v.idx, t.buf, v.buf, false, routine.Pow)
+
 	return t
 }
 
-// Sqrt TODO.
+// Sqrt computes square root of all tensor elements.
 func (t *Tensor) Sqrt() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Sqrt)
+
 	return t
 }
 
-// Log TODO.
+// Log computes natural logarithm of all tensor elements.
 func (t *Tensor) Log() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Log)
+
 	return t
 }
 
-// Log10 TODO.
+// Log10 computes decimal logarithm of all tensor elements.
 func (t *Tensor) Log10() *Tensor {
+	t.init()
+
+	math.Nullary(t.idx, t.buf, false, routine.Log10)
+
 	return t
 }
